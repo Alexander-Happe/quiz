3ce1be250b2291a699d1924d308f14f59df24efb
Alex Happe <abhdiver1994@gmail.com>
2019-11-02 16:30:55 -0500

    adds what i could

---
+++ assets/questions.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        title: "What is the C in CSS?",
+        choices: ["Cool", "Cascading", "Clever", "Clingy"],
+        answer: "Cascading"    
+    },
+    {
+        title: "What can't you do with javascript?",
+        choices: ["Animate things", "add function", "Add sound effects", "Cry"],
+        answer: "Cry"
+    },
+    {
+        title: "What does the M stand for in HTML",
+        choices: ["Makeup", "Markup", "Markdown", "Mellisa"],
+        answer: "Markup"
+    },
+    {
+        title: "What question is this?",
+        choices: ["1", "5", "3", "4"],
+        answer: "4"
+    },
+    {
+        title: "Should you do your homework last minute?",
+        choices: ["Yes", "Maybe", "No", "Who's asking?"],
+        answer: "No"
+    }
+]
--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,102 @@
 var timer = document.querySelector(".timer")
 var header = document.querySelector(".head")
-var info = document.querySelector(".info")
+var info = document.getElementById("info")
 var startbttn = document.getElementById("start-bttn")
 var highbttn = document.getElementById("score-bttn")
+var hide = document.querySelectorAll(".stuff")
+var bttndiv = document.getElementById("button")
+
+
+
 
 var secondsleft = 75;
-var secondselapsed = 0;
 var interval;
+var answer = ""
 
 function starttime(){
-    
     interval = setInterval(function(){
         secondsleft--;
         displaytime()
         if (secondsleft <= 0)
             clearInterval(interval)
-        console.log(secondsleft)
     }, 1000)
 }
 function displaytime(){
     timer.textContent = "Time: " + secondsleft
 }
-starttime()
+function gamestart(){
+    starttime()
+    for (i=0;i<hide.length; i++){
+        hide[i].className = "hide"
+    }
+    if (answer === ""){
+        var question = document.createElement("h2")
+        question.className = "info" 
+        question.innerHTML = questions[0].title
+        info.append(question)
+        for (i=0;i<4; i++){
+            var buttons = document.createElement("button")
+            buttons.className = "questionbttn"
+            buttons.innerHTML = questions[0].choices[i]
+            buttons.setAttribute("value", questions[0].choices[i])
+            bttndiv.append(buttons)
+        }
+    }
+    var newbttns = document.querySelectorAll(".questionbttn")
+    
+    console.log(newbttns)
+    for (i=0;i<newbttns.length;i++){
+        console.log(newbttns[i].value)
+        newbttns[i].addEventListener("click", function(){
+            if (this.value === questions[0].answer){
+                console.log(this.value)
+                answer = "1"
+                console.log(answer)
+            }
+            else {
+                secondsleft = secondsleft - 10
+                alert("Wrong")
+            }
+        })
+    }
+    if (answer === "1"){
+        bttndiv.remove(buttons)
+        info.remove(question)
+        var question = document.createElement("h2")
+        question.className = "info" 
+        question.innerHTML = questions[1].title
+        info.append(question)
+        for (i=0;i<4; i++){
+            var buttons = document.createElement("button")
+            buttons.className = "questionbttn"
+            buttons.innerHTML = questions[1].choices[i]
+            buttons.setAttribute("value", questions[1].choices[i])
+            bttndiv.append(buttons)
+        }
+    }
+    var newbttns = document.querySelectorAll(".questionbttn")
+    
+    console.log(newbttns)
+    for (i=0;i<newbttns.length;i++){
+        console.log(newbttns[i].value)
+        newbttns[i].addEventListener("click", function(){
+            if (this.value === questions[1].answer){
+                console.log(this.value)
+                answer = "2"
+            }
+            else {
+                secondsleft = secondsleft - 10
+                alert("Wrong")
+            }
+        })
+    }
+}
+
+ 
+   
+
 startbttn.addEventListener("click", function(){
-    //start timer at 75 sec
+    gamestart()
 })
 highbttn.addEventListener("click", function(){
     //transfer to high score page w/ local storage log
